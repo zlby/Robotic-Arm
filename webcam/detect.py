a02a9595a8da140825df1ee9887aa20845b365aa
周梁博雅 <zlby970129@gmail.com>
2018-07-31 18:37:58 +0100

    webcame 获取图片

---
+++ webcam/detect.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+i = 1
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if (i <= 100) & (i % 10 == 0):
+        cv2.imwrite(str(i) + '.jpg', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    i += 1
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
