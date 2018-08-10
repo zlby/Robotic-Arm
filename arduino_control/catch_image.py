3379e992293666a2af1d323bd198c793fa67ed36
周梁博雅 <zlby970129@gmail.com>
2018-08-10 15:03:16 +0100

    return the position of box by function get_position_of_box

--- arduino_control/catch_image.py
+++ arduino_control/catch_image.py
@@ -1,15 +1,37 @@
 from arduino_control.yolov3.yolo_video import make_detection
 from arduino_control.yolov3.yolo import YOLO, detect_video
 from PIL import Image
-from pyzbar.pyzbar import decode
+from pyzbar import pyzbar
 
-boxes = make_detection(YOLO(), 'qrcode.jpg')
+def get_position_of_box(message):
 
-for box in boxes:
-    if box['class'] != 'cup':
-        continue
-    print(box['class'])
-    im = Image.open('qrcode.jpg')
-    region = im.crop((box['left'], box['top'], box['right'], box['bottom']))
-    region.save('box.jpg')
-    print(decode(Image.open('box.jpg')))
+    # using yolov3 to detect cups
+    boxes = make_detection(YOLO(), 'object.jpg')
+
+    for box in boxes:
+        if box['class'] != 'cup':
+            continue
+        im = Image.open('object.jpg')
+        region = im.crop((box['left'], box['top'], box['right'], box['bottom']))
+        # region.save('box.jpg')
+        detect_objs = pyzbar.decode(region)
+
+        # if there is no box detected, return -1
+        if len(detect_objs) == 0:
+            continue
+
+        # detect qr code information
+        qrdata = str(detect_objs[0].data, encoding='utf-8')
+
+        # if the qrcode is not what we want, go ahead to another one
+        if qrdata != message:
+            continue
+
+        middle_point  = int(round((box['left'] + box['right']) / 2))
+
+        return middle_point
+
+    return -1
+
+if __name__ == '__main__':
+    print(get_position_of_box('drug A'))
